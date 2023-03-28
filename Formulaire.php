5b5212e572d43e74cf86a3038d05149c1f4e5a1e
MatthiasBND <125035549+MatthiasBND@users.noreply.github.com>
2023-03-28 17:28:28 +0200

    avancement du 28/03

--- Formulaire.php
+++ Formulaire.php
@@ -1,42 +1,33 @@
 <?php
 // Paramètres de connexion à la base de données
-
 $host = 'localhost';
 $username = 'ArmadaAdmin';
 $password = 'btssnir';
 $dbname = 'armada_projet';
 
-
 // Connexion à la base de données
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
 // Vérifier la connexion
-if (!$conn->connect_error) {
+if ($conn->connect_error) {
     die("Connexion échouée: " . mysqli_connect_error());
 }
 echo "Connexion réussie";
 
+$user_nom = $_POST["user_nom"];
+$user_prenom = $_POST["user_prenom"];
+$user_datenaissance = $_POST["user_datenaissance"];
+$user_adressemail = $_POST["user_adressemail"];
 
+$reqUti = "INSERT INTO utilisateur (nom, prenom, dateDeNaissance, adresseMail) VALUES ('$user_nom', '$user_prenom', '$user_datenaissance', '$user_adressemail')";
+$reqRes = "INSERT INTO reservation (nom, prenom, dateDeNaissance, adresseMail) VALUES ('$user_nom', '$user_prenom', '$user_datenaissance', '$user_adressemail')";
 
-    $user_nom =  $_POST["user_nom"];
-    $user_prenom =  $_POST["user_prenom"];
-    $usernombateau =  $_POST["user_nombateau"];
-    $usernombateau =  $_POST["user_datenaissance"];
-    $usernombateau =  $_POST["user_adressemail"];
+if ($conn->query($reqUti) === TRUE && $conn->query($reqRes) === TRUE) {
+    echo "Votre message a été envoyé avec succès.";
+} else {
+    echo "Erreur : " . $conn->error;
+}
 
-    $reqUti = "INSERT INTO utilisateur (nom, prenom, dateDeNaissance,adresseMail) VALUES ('$user_nom', '$user_prenom', '$user_datenaissance','$user_adressemail')";
-    $reqRes = "INSERT INTO reservation (nom, prenom, dateDeNaissance,adresseMail) VALUES ('$user_nom', '$user_prenom', '$user_datenaissance','$user_adressemail')";
-
-    $sql = "INSERT INTO reservation select from utilisateur id";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "Votre message a été envoyé avec succès.";
-    } 
-    
-    else {
-        echo "Erreur : " . $sql . "<br>" . $conn->error;
-    }
-
-    $conn->close();
-
+$conn->close();
 ?>
+
