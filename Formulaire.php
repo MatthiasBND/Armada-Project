69e5d42511ba8441ba36821c44d0427f07a4bfd8
MatthiasBND <125035549+MatthiasBND@users.noreply.github.com>
2023-05-09 17:20:35 +0200

    progression du 9/05

--- Formulaire.php
+++ Formulaire.php
@@ -6,14 +6,22 @@ $password = 'btssnir';
 $dbname = 'armada_projet';
 
 if (!empty($_POST)) {
+    /*Teste si le tableau $_POST contient des données soumises 
+    par le formulaire. */
+
+
     $user_nom = $_POST["user_nom"];
     $user_prenom = $_POST["user_prenom"];
     $user_datenaissance = $_POST["user_datenaissance"];
     $user_adressemail = $_POST["user_adressemail"];
-
+    /*Récupération des données soumises par le formulaire 
+    dans des variables */
+    
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        /*Création d'un objet PDO pour 
+        la connexion à la base de données.*/
 
         $sql = "INSERT INTO utilisateur (nom, prenom, dateDeNaissance, adresseMail) VALUES (:value1, :value2, :value3, :value4)";
 
@@ -23,11 +31,19 @@ if (!empty($_POST)) {
         $statement->bindParam(":value3", $user_datenaissance);
         $statement->bindParam(":value4", $user_adressemail);
         $statement->execute();
+        /*Préparation de la requête d'insertion SQL avec des paramètres nommés et liaison des valeurs des variables récupérées à ces paramètres.
+         Exécution de la requête avec la méthode execute().*/
+
         echo "Données insérées avec succès.";
-    } catch (PDOException $e) {
+
+
+    } 
+    
+    catch (PDOException $e) {
         echo "Erreur : " . $e->getMessage();
     }
-
+    /*Capture d'une exception PDO en cas d'erreur de connexion à la base de données ou d'erreur d'exécution de la requête SQL
+     et affichage d'un message d'erreur correspondant.*/
     
 }
 ?>
